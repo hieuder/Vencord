c71d648410a12e332fb4c3d62316fa89eb08cc51
hieuder <hieudrm@gmail.com>
2024-12-26 17:55:33 +0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -579,6 +579,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "jamesbt365",
         id: 158567567487795200n,
     },
+    Balaclava: {
+        name: "Balaclava",
+        id: 854886148455399436n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
